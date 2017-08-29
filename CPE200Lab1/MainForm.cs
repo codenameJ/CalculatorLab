b7621585cfedafa89f016a59ec6445d950a6f35b
J Minimalist <J Minimalist>
2017-08-29 15:07:47 +0700

    Add project files.

---
+++ CPE200Lab1/MainForm.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CPE200Lab1
+{
+    public partial class MainForm : Form
+    {
+        private bool hasDot;
+        private bool isAllowBack;
+        private bool isAfterOperater;
+        private bool isAfterEqual;
+        private string firstOperand;
+        private string operate;
+
+        CalculatorEngine engine;
+
+        private void resetAll()
+        {
+            lblDisplay.Text = "0";
+            isAllowBack = true;
+            hasDot = false;
+            isAfterOperater = false;
+            isAfterEqual = false;
+            firstOperand = null;
+        }
+
+        public MainForm()
+        {
+            engine = new CalculatorEngine();
+            InitializeComponent();
+
+            resetAll();
+        }
+
+        private void btnNumber_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text is "Error")
+            {
+                return;
+            }
+            if (isAfterEqual)
+            {
+                resetAll();
+            }
+            if (isAfterOperater)
+            {
+                lblDisplay.Text = "0";
+            }
+            if(lblDisplay.Text.Length is 8)
+            {
+                return;
+            }
+            isAllowBack = true;
+            string digit = ((Button)sender).Text;
+            if(lblDisplay.Text is "0")
+            {
+                lblDisplay.Text = "";
+            }
+            lblDisplay.Text += digit;
+            isAfterOperater = false;
+        }
+
+        private void btnOperator_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text is "Error")
+            {
+                return;
+            }
+            if (isAfterOperater)
+            {
+                return;
+            }
+            string kep = operate;
+            operate = ((Button)sender).Text;
+            if (firstOperand != null && operate != "%" )
+            {
+                string secondOperand = lblDisplay.Text;
+                string result = engine.calculate(operate, firstOperand, secondOperand);
+                if (result is "E" || result.Length > 8)
+                {
+                    lblDisplay.Text = "Error";
+                }
+                else
+                {
+                    lblDisplay.Text = result;
+                }
+                firstOperand = lblDisplay.Text;
+                isAfterOperater = true;
+            }
+            else 
+            {
+                switch (operate)
+                {
+                    case "+":
+                    case "-":
+                    case "X":
+                    case "÷":
+                        firstOperand = lblDisplay.Text;
+                        isAfterOperater = true;
+                        break;
+                    case "%":
+                        string sum;
+                        sum = (Convert.ToDouble(firstOperand) * (Convert.ToDouble(lblDisplay.Text) / 100)).ToString();
+                        lblDisplay.Text = sum;
+                        operate = kep;
+                        break;
+                }
+                isAllowBack = false;
+            }
+        }
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text is "Error")
+            {
+                return;
+            }
+            string secondOperand = lblDisplay.Text;
+            string result = engine.calculate(operate, firstOperand, secondOperand);
+            if (result is "E" || result.Length > 8)
+            {
+                lblDisplay.Text = "Error";
+            }
+            else
+            {
+                lblDisplay.Text = result;
+            }
+            isAfterEqual = true;
+        }
+
+        private void btnDot_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text is "Error")
+            {
+                return;
+            }
+            if (isAfterEqual)
+            {
+                resetAll();
+            }
+            if (lblDisplay.Text.Length is 8)
+            {
+                return;
+            }
+            if (!hasDot)
+            {
+                lblDisplay.Text += ".";
+                hasDot = true;
+            }
+        }
+
+        private void btnSign_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text is "Error")
+            {
+                return;
+            }
+            if (isAfterEqual)
+            {
+                resetAll();
+            }
+            // already contain negative sign
+            if (lblDisplay.Text.Length is 8)
+            {
+                return;
+            }
+            if(lblDisplay.Text[0] is '-')
+            {
+                lblDisplay.Text = lblDisplay.Text.Substring(1, lblDisplay.Text.Length - 1);
+            } else
+            {
+                lblDisplay.Text = "-" + lblDisplay.Text;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            resetAll();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            if (lblDisplay.Text is "Error")
+            {
+                return;
+            }
+            if (isAfterEqual)
+            {
+                return;
+            }
+            if (!isAllowBack)
+            {
+                return;
+            }
+            if(lblDisplay.Text != "0")
+            {
+                string current = lblDisplay.Text;
+                char rightMost = current[current.Length - 1];
+                if(rightMost is '.')
+                {
+                    hasDot = false;
+                }
+                lblDisplay.Text = current.Substring(0, current.Length - 1);
+                if(lblDisplay.Text is "" || lblDisplay.Text is "-")
+                {
+                    lblDisplay.Text = "0";
+                }
+            }
+        }
+    }
+}
